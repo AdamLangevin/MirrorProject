59611b919fe66af7991be5d32e017d54763b7680
Adam Langevin <langer1911@gmail.com>
2017-03-30 16:08:44 -0400

    helpers

--- views/js/controllers/index.js
+++ views/js/controllers/index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 'use strict';
 
 var app = require('angular').module('mirror');
 
 app.controller('MainController', require('./main'));
-=======
-'use strict';
-
-var app = require('angular').module('mirror');
-
-app.controller('MainController', require('./main'));
->>>>>>> a989be0d0c98ee64c41265ab02bab13979bc69c4
